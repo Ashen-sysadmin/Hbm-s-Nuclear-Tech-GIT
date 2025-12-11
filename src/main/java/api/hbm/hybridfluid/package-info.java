338f2ee7b4b37d602c21250b968fa77bf7594747
Ashen-Sysadmin <65367913+MerrittK@users.noreply.github.com>
2025-12-11 17:05:17 -0500

    GRADLE

---
+++ src/main/java/api/hbm/hybridfluid/package-info.java
@@ -0,0 +1,11 @@
+package api.hbm.hybridfluid;
+
+/*
+I'm not an expert coder by any means, but I want to make ntm a cornerstone of a modpack development kit, so hear me out.
+
+Hybrid tanks could easily implement forge tank methods and work from there - that part isn't actually hard.
+
+What is difficult is all the little bits and pieces that go into interpolating between two very different systems.
+
+I'm going to start with making a registry to register sane conversions between ntm and forge fluids - I think by creating a hashmap of ntm fluids against forge fluids or smth.
+*/
